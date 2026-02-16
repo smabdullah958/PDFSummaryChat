a0aebab4e5c5e654a7fadb789e35968a3ba96149
smabdullah958 <smabdulla958@gmail.com>
2026-02-17 02:50:08 +0500

    update the version of a next

--- SummaryGeneratorFrontend/app/loading.jsx
+++ SummaryGeneratorFrontend/app/loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loader = () => {
   return (
-    <div className="flex justify-center items-center h-screen bg-yellow-100">
+    <div className="flex justify-center items-center h-screen">
       <div className="w-16 h-16 border-4 border-blue-500 border-dashed rounded-full animate-spin 3xl:w-24 3xl:h-24"></div>
     </div>
   );
