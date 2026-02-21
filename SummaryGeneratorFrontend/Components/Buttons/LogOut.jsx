6132734705a56867e5cf49b55aa87efa74ca30b0
smabdullah958 <smabdulla958@gmail.com>
2026-02-22 00:53:46 +0500

    improve button visibiliY'

--- SummaryGeneratorFrontend/Components/Buttons/LogOut.jsx
+++ SummaryGeneratorFrontend/Components/Buttons/LogOut.jsx
@@ -37,7 +37,7 @@ const LogOut = () => {
         <button
           onClick={HandleButton}
           disabled={Loading}
-          className={`px-6 py-2 md:mr-10 rounded-xl  bg-gradient-to-r from-[#3fb7eb] to-[#23a4dc] text-white font-semibold shadow-lg  transition-all duration-500 transform hover:scale-105 active:scale-95  ${IsLogIn ? "opacity-100 hover:from-[#0693cf] hover:to-[#0f94cd]" : "opacity-0"} ${Loading ? "opacity-40 cursor-not-allowed bg-gray-200" : ""}`}
+          className={`px-6 py-2 md:mr-10 rounded-xl   text-white font-semibold shadow-lg  transition-all duration-500 transform hover:scale-105 active:scale-95  ${IsLogIn ? "opacity-100 hover:from-[#0693cf] hover:to-[#0f94cd]" : "opacity-0"} ${Loading ? "opacity-0 cursor-not-allowed bg-gradient-to-r from-[#83c2dc] to-[#8dc4db]" : "bg-gradient-to-r from-[#3fb7eb] to-[#23a4dc]"}`}
         >
           {Loading ? <ButtonLoader /> : "Log Out"}
         </button>
